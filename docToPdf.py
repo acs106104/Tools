9b9e3997c014752609bbe6d2a547975d624c2aac
acs106104 <acs106104@gm.ntcu.edu.tw>
2022-10-27 15:28:46 +0800

    adds remove func --d
    add --d in command to delete the .docx file at the end of program

--- docToPdf.py
+++ docToPdf.py
@@ -1,10 +1,11 @@
 from docx2pdf import convert
-import glob
+import glob, os
 from pathlib import Path
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filedir",help ="original file path")
+parser.add_argument("--d", help="delete docx files", action='store_true')
 
 args = parser.parse_args()
 filedir = args.filedir
@@ -17,3 +18,7 @@ for file in FileList:
     filename = str(file).split(".")[0]
     # print(filename)
     convert(file,f"{filename}.pdf")
+
+if args.d == True:
+    for file in FileList:
+        os.remove(file)
